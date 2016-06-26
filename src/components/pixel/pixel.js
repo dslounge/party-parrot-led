3c85bf0bb78223c4c277274229eb7bbb2f703d4a
Rafael Mendiola <dslounge@gmail.com>
2016-06-25 23:41:06 -0400

    style pixels. Attempt at using a font-data file.

--- src/components/pixel/pixel.js
+++ src/components/pixel/pixel.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import classnames from 'classnames';
 require('./pixel-style.css');
 
-const Pixel = () => {
+const Pixel = (props) => {
+  const className = classnames('pixel', { on: props.on, off: !props.on });
+
   return (
-    <div className="pixel" />
+    <div className={className} />
   );
+};
+
+Pixel.propTypes = {
+  on: React.PropTypes.bool,
 };
 
 export default Pixel;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pixel from './components/pixel/pixel';
+import font from './font-data';
+
 const TestApp = () => {
+  const character = font['&'];
+  const matrix = character.map((row) => parseInt(row, 16).toString(2).split(''));
+
+  const rows = matrix.map((row) => {
+    const pixels = row.map((val) => {
+      return (
+        <Pixel on={val === '1'} />
+      );
+    });
+
+    return (
+      <div>
+        {pixels}
+      </div>
+    );
+  });
+
   return (
     <div>
-      <h1>SUP!</h1>
-      <Pixel />
+      <h1>Pixel test!</h1>
+      {rows}
     </div>
   );
 };
