46c277b4321763921e0a2c40479815598df08575
Rafael Mendiola <dslounge@gmail.com>
2016-06-26 15:31:43 -0400

    Fix the conversion of font data to matrix.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import Pixel from './components/pixel/pixel';
 import font from './font-data';
 
 const TestApp = () => {
-  const character = font['&'];
-  const matrix = character.map((row) => parseInt(row, 16).toString(2).split(''));
+  const character = font['#'];
+  const matrix = character.map((row) => _.padStart(row.toString(2), 5, '0').split(''));
 
   const rows = matrix.map((row) => {
     const pixels = row.map((val) => {
