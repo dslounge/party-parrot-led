e2c7104fb3adfb7d00b31bb397e77d5416638ffc
Rafael Mendiola <dslounge@gmail.com>
2016-06-25 22:57:50 -0400

    Created a pixel component

---
+++ src/components/pixel/pixel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+require('./pixel-style.css');
+
+const Pixel = () => {
+  return (
+    <div className="pixel" />
+  );
+};
+
+export default Pixel;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Pixel from './components/pixel/pixel';
 const TestApp = () => {
   return (
     <div>
       <h1>SUP!</h1>
+      <Pixel />
     </div>
   );
 };
